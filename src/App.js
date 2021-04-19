84f63ac284d9520371660c0212d61c3f4de7d873
Hyun-Joo <dlguswn714@gmail.com>
2021-04-19 20:55:15 +0900

    Custom Hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useRef, useReducer, useMemo, useCallback} from 'react';
 // useMemo, useCallback, React.memo : 정말 최적화가 필요할 때만 사용하는 게 좋음
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
+import useInputs from "./js/useInputs";
 
 function countActiveUsers(users) {
     console.log('활성 사용자 수를 세는 중...');
@@ -9,10 +10,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-    inputs: {
-        username: '',
-        email: ''
-    },
     users: [
         {
             id: 1,
@@ -37,14 +34,6 @@ const initialState = {
 
 function reducer(state, action) {
     switch (action.type) {
-        case 'CHANGE_INPUT':
-            return {
-                ...state,
-                inputs: {
-                    ...state.inputs,
-                    [action.name]: action.value
-                }
-            };
         case 'CREATE_USER':
             return {
                 inputs: initialState.inputs,
@@ -71,18 +60,12 @@ function reducer(state, action) {
 
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const [form, onChange, reset] = useInputs({
+        username:'', email: ''
+    });
+    const { username, email } = form;
     const nextId = useRef(4);
     const {users} = state;
-    const {username, email} = state.inputs;
-
-    const onChange = useCallback(e => {
-        const {name, value} = e.target;
-        dispatch({
-            type: 'CHANGE_INPUT',
-            name,
-            value
-        });
-    }, []);
 
     const onCreate = useCallback(() => {
         dispatch({
@@ -94,7 +77,8 @@ function App() {
             }
         });
         nextId.current += 1;
-    }, [username, email]);
+        reset();
+    }, [username, email, reset]); // esLint 규칙상 reset 추가
 
     const onToggle = useCallback(id => {
         dispatch({
