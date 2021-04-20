5a6f2e4115854f70177351daddb83c687d13cc39
Hyun-Joo <dlguswn714@gmail.com>
2021-04-20 12:58:28 +0900

    Custom Hook 2

--- src/js/useInputs.js
+++ src/js/useInputs.js
@@ -1,12 +1,32 @@
-import { useState, useCallback } from "react";
+import {useReducer, useCallback} from "react";
 
-function useInputs(initialForm){
-    const [form, setForm] = useState(initialForm);
-    const onChange = useCallback((e = window.event) => {
-        const { name, value } = e.target;
-        setForm(form => ({ ...form, [name]: value }));
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            return {...state, [action.name]: action.value};
+        case 'RESET_INPUT':
+            return Object.keys(state).reduce((acc, curr) => {
+                acc[curr] = '';
+                return acc;
+            }, {});
+        default:
+            return state;
+    }
+}
+
+function useInputs(initialForm) {
+    const [form, dispatch] = useReducer(reducer, initialForm);
+
+    const onChange = useCallback(e => {
+        const {name, value} = e.target;
+        dispatch({
+            type: 'CHANGE_INPUT',
+            name,
+            value
+        });
     }, []);
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+
+    const reset = useCallback(() => dispatch({type: 'RESET_INPUT'}), []);
 
     return [form, onChange, reset];
 }
