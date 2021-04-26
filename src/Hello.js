a4b135a0ce8ba3aa95938f0b695e11fee20e8c7c
Hyun-Joo <dlguswn714@gmail.com>
2021-04-26 21:34:14 +0900

    class형 컴포넌트

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Hello({ color, name, isSpecial }){ // 비구조화 할당(props 대신...)
-    console.log(color, name);
-    return (
-        <div style={{color}}>{/* 객체 형태 => {color: color} */}
-            {/* 삼항연산자 대신 && 사용(단순 보여줌 or 숨김시) */}
-            {isSpecial && <b>*</b>}
-            사랑의 배신자여, {name}
-        </div>);
+class Hello extends Component {
+    static defaultProps = {
+        name: '무명'
+    };
+    render(){
+        const { color, isSpecial, name } = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}님
+            </div>
+        )
+    }
 }
 
-Hello.defaultProps = {
-    name: '무명'
-}
+// function Hello({ color, name, isSpecial }){ // 비구조화 할당(props 대신...)
+//     console.log(color, name);
+//     return (
+//         <div style={{color}}>{/* 객체 형태 => {color: color} */}
+//             {/* 삼항연산자 대신 && 사용(단순 보여줌 or 숨김시) */}
+//             {isSpecial && <b>*</b>}
+//             사랑의 배신자여, {name}
+//         </div>);
+// }
+
+// Hello.defaultProps = {
+//     name: '무명'
+// }
+
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from "./Hello";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+    <Hello name="react" isSpecial />,
   document.getElementById('root')
 );
 
